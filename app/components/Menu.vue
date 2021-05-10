87eb00122908803099558021c10af05350565e5c
Karl Schmaltz <karlschmaltz@gmail.com>
2021-05-10 05:57:25 -0600

    feat: update vue demo

---
+++ app/components/Menu.vue
@@ -0,0 +1,50 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Vue.js Demo" />
+        </ActionBar>
+
+        <ScrollView>
+            <StackLayout>
+                <StackLayout v-for="demo in demos" :key="demo.name">
+                    <Button :text="demo.name" @tap="goToDemo(demo.component)" />
+                </StackLayout>
+            </StackLayout>
+        </ScrollView>
+    </Page>
+</template>
+
+<script lang="ts">
+import { demos } from './linked-components/install';
+declare const isDevelopment: boolean;
+
+export default {
+    data() {
+        return {
+            demos
+        };
+    },
+    mounted() {
+        if (isDevelopment) {
+            const Development = demos.find(({ name }) => name === 'Development');
+            if (Development) {
+                this.$navigateTo(Development.component, {
+                    animated: false
+                });
+            }
+        }
+    },
+    methods: {
+        goToDemo(component) {
+            this.$navigateTo(component, {
+                animated: true,
+                transition: {
+                    name: 'slideLeft',
+                    duration: 200,
+                    curve: 'ease'
+                }
+            });
+        }
+    }
+};
+</script>
